e084d6dc13b039c30fa431086ef40a89c73c0728
Thomas Steifler <steiflerthomas@gmail.com>
2024-07-27 18:26:03 +0200

    feat: add header content to cv preview

--- src/components/CVViewer.jsx
+++ src/components/CVViewer.jsx
@@ -2,14 +2,23 @@
 
 import styles from "@styles/cvViewer.module.css";
 
-/** @param {Object} param0
- * @param {CVData} param0.data  */
+/**
+ * @param {Object} param0
+ * @param {CVData} param0.data
+ *
+ * @returns {JSX.Element}
+ */
 export default function CVViewer({ data }) {
-    console.log(styles)
+    const { general, education, experience, skills, languages } = data;
 
     return (
         <div className={styles["cv-viewer"]}>
-            <div className={styles.header}></div>
+            <div className={styles.header}>
+                <h1>
+                    {general.firstName} {general.lastName}
+                </h1>
+                <p>{general.title}</p>
+            </div>
             <div className={styles.sidebar}></div>
             <div className={styles.content}></div>
         </div>
